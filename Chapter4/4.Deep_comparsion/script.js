15008f622629212b5e4790b6b3dee4dd73f00b14
proskuryakoff <misha.proskuryakov.99@gmail.com>
2021-07-07 02:25:59 +0300

    v2

--- Chapter4/4.Deep_comparsion/script.js
+++ Chapter4/4.Deep_comparsion/script.js
@@ -1,20 +1,26 @@
 function deepEqual(x,y){
-    if (typeof x == "object" && x!= null && typeof y == "object" && y!= null){
-        keyX = Object.keys(x);
-        keyY = Object.keys(y);
-        if (keyX.length !== keyY.length) return false;
-        for(let i of keyX){
-            if (!keyX.includes (i)) return false;
-            if (!deepEqual(x[i], y[i])) return false;
-            else return true
-            }
-        }
     if (x === y) return true;
+    if (typeof x == "object" && x != null && typeof y == "object" && y != null){
+        let keyX = Object.keys(x),
+            keyY = Object.keys(y);
+        if (keyX.length != keyY.length) return false;
+        for(let i of keyX){
+            if (!keyY.includes(i) || !deepEqual(x[i], y[i])) return false;
+        }
+        return true;
+    } 
+    return false;
 }
 
-
-
 let obj = {here: {is: "an"}, object: 2};
+
+// TEST
 console.log(deepEqual(obj, obj));
 console.log(deepEqual(obj, {here: 1, object: 2}));
-console.log(deepEqual(obj, {here: {is: "an"}, object: 4}));
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+console.log(deepEqual(obj, "here"));
+console.log(deepEqual("here", "here"));
+console.log(deepEqual("123", "here"));
+console.log(deepEqual(123, 12));
+console.log(deepEqual(123, "12"));
+console.log(deepEqual(123, 123));
