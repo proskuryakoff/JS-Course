155023e1b4db997d7378575e690edb56462ba94c
proskuryakoff <misha.proskuryakov.99@gmail.com>
2021-07-18 23:23:50 +0300

    v1

---
+++ Chapter6/2. Groups/script.js
@@ -0,0 +1,35 @@
+console.log ('Groups');
+
+class Group {
+    constructor(){
+        this.array = [];
+    }
+    add(value){
+        if(!this.array.includes(value)) {
+            this.array.push(value)
+        };
+    }
+    delete(value){
+        if(this.array.includes(value)){ 
+            let result = this.array.filter(element => element !== value);
+            this.array = result;
+        }
+    }
+    has(value){
+        return this.array.includes(value);
+    }
+    static from(arr){
+        let newGroup = new Group;
+        for(let elem of arr){
+            newGroup.add(elem);
+        }
+        return newGroup;
+    }
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+console.log(group.has(30));
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
