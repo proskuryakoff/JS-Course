861a7e2161a8c1902ea524b999b189e12076e28a
proskuryakoff <misha.proskuryakov.99@gmail.com>
2021-08-15 14:09:04 +0300

    v1

--- Chapter8/2.The locked box/script.js
+++ Chapter8/2.The locked box/script.js
@@ -1,2 +1,39 @@
 console.log ('The locked box');
 
+const box = {
+    locked: true,
+    unlock() { this.locked = false; },
+    lock() { this.locked = true;  },
+    _content: [],
+    get content() {
+      if (this.locked) throw new Error("Locked!");
+      return this._content;
+    }
+  };
+  
+  function withBoxUnlocked(body) {
+    try{
+        if (!box.locked) return body();
+        else {
+            box.unlock();
+            return body();
+        }
+    }
+    finally{
+        box.lock();
+    } 
+  }
+  
+  withBoxUnlocked(function() {
+    box.content.push("gold piece");
+    console.log(box._content)
+  });
+  
+  try {
+    withBoxUnlocked(function() {
+      throw new Error("Pirates on the horizon! Abort!");
+    });
+  } catch (e) {
+    console.log("Error raised: " + e);
+  }
+  console.log(box.locked);
